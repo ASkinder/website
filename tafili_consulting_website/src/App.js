6d84808725214a7b7f50cbdf1cd41ce30366c7a4
maxRechem <mvr@tafiliconsulting.be>
2024-01-29 10:34:04 +0100

    start

--- tafili_consulting_website/src/App.js
+++ tafili_consulting_website/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Link
+          Learn React
         </a>
       </header>
     </div>

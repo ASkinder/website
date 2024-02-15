27a68748cbef7fca6959c10fb3d2652d2e380c33
ASkinder <ato@tafiliconsulting.be>
2024-02-15 14:16:35 +0100

    deploy
    audit fix

---
+++ tafili_consulting_website/build/precache-manifest.0bcb8db5ab78d42dcecd7f64a3daffde.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fa5ebd21a27288a86040c0f87b146e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "931e528fd7aa10ab7811",
+    "url": "/static/css/main.82ee3efc.chunk.css"
+  },
+  {
+    "revision": "ceb67f4695932c080a4a",
+    "url": "/static/js/2.8a398771.chunk.js"
+  },
+  {
+    "revision": "ab86aee8a7fefdb128ac90888f888161",
+    "url": "/static/js/2.8a398771.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48603bd7f6f48bf56058",
+    "url": "/static/js/3.7761cbeb.chunk.js"
+  },
+  {
+    "revision": "931e528fd7aa10ab7811",
+    "url": "/static/js/main.e5eb21c2.chunk.js"
+  },
+  {
+    "revision": "d88a7a29742ea03accd9",
+    "url": "/static/js/runtime-main.95973325.js"
+  },
+  {
+    "revision": "25a9d9c0236eaa81c09998e9902bf300",
+    "url": "/static/media/LogoTafili.25a9d9c0.png"
+  },
+  {
+    "revision": "e48b6c8927b09d3f5fde7fb53168bc7c",
+    "url": "/static/media/TrustCompany.e48b6c89.png"
+  },
+  {
+    "revision": "43b1c5f0878c12277db53b91d4f8d62d",
+    "url": "/static/media/buildingCam.43b1c5f0.jpg"
+  },
+  {
+    "revision": "89a6a09e049ab6cb2d7fefe552fdcfbb",
+    "url": "/static/media/businessCentral.89a6a09e.jpg"
+  },
+  {
+    "revision": "29490e691bd2f1972def4f007e4ec40a",
+    "url": "/static/media/yetoo_illu2.29490e69.jpg"
+  }
+]);

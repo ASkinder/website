fb31ec60f3be16615fb12e35ff27a8a160acd653
ASkinder <ato@tafiliconsulting.be>
2024-02-16 14:24:51 +0100

    Updates

---
+++ precache-manifest.1a863bf010c7b9c7469f5cd0f5df978e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e6d69898594a28a91f263a1895990a9",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "54248337d8d1e4fd0f93",
+    "url": "/website/static/css/2.cc82f8aa.chunk.css"
+  },
+  {
+    "revision": "88a1d352b8b3f55411a4",
+    "url": "/website/static/css/main.c597d1f2.chunk.css"
+  },
+  {
+    "revision": "54248337d8d1e4fd0f93",
+    "url": "/website/static/js/2.6752b6d9.chunk.js"
+  },
+  {
+    "revision": "a7f47c0c8a79ddc05680d36cdf8cdd10",
+    "url": "/website/static/js/2.6752b6d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0378f4fa89bf919c6eea",
+    "url": "/website/static/js/3.39a7989e.chunk.js"
+  },
+  {
+    "revision": "88a1d352b8b3f55411a4",
+    "url": "/website/static/js/main.a841c614.chunk.js"
+  },
+  {
+    "revision": "b6abd01f9faeea3600aa",
+    "url": "/website/static/js/runtime-main.3969564f.js"
+  },
+  {
+    "revision": "25a9d9c0236eaa81c09998e9902bf300",
+    "url": "/website/static/media/LogoTafili.25a9d9c0.png"
+  },
+  {
+    "revision": "f94bf897cbc21a056c5ec17a3c73aadd",
+    "url": "/website/static/media/MicrosoftBusinessCentral.f94bf897.mp4"
+  },
+  {
+    "revision": "6736374d0731b381f18cad103afc2b33",
+    "url": "/website/static/media/companies.6736374d.png"
+  },
+  {
+    "revision": "999979e6724a0a1841368a7b69aaf2d5",
+    "url": "/website/static/media/office1.999979e6.png"
+  },
+  {
+    "revision": "8df94f5640f3b6db49a87bfefa1c6979",
+    "url": "/website/static/media/office2.8df94f56.png"
+  },
+  {
+    "revision": "4541f14547f76435b75e150542bcc7cb",
+    "url": "/website/static/media/office3.4541f145.png"
+  }
+]);

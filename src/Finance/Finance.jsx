53f10398917145fa08e4f4b8899a3eeb15b1cf72
maxRechem <mvr@tafiliconsulting.be>
2024-02-26 15:19:28 +0100

    mbc

---
+++ src/Finance/Finance.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import './Finance.css';
+import data_analyst from "../Img/data_analyst.png"
+import bc_illu from "../Img/business_controller_illu.png"
+
+function Finance() {
+    return (
+        <div className="Finance">
+            <div className="Finance1">
+                <br/> <br/> <br/> <br/>
+                <div className="image_finance">
+                    <br/><br/><br/><br/>
+                    <div className="financial_title">FINANCIAL ANALYST</div>
+                    <br/><br/><br/>
+                    <div className="financial_text">How can your Finance function boost sales,
+                        control costs, reduce risk and improve
+                        sustainability? With the help of financial management software.
+                    </div>
+                </div>
+                <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+                <h2 className="data_title">Data analyst</h2>
+                <br/><br/><br/><br/><br/><br/>
+                <div className="data_analyst">
+                    <img src={data_analyst} className="data_image" alt="data analyst illustration"/>
+                    <div className="data_textes">
+                        <span className="data_text">
+                            Data Analysts interpret complex data sets to provide actionable insights, aiding
+                            organizations in decision-making. They collect, process, and analyze data using statistical
+                            methods and programming languages like Python or R to uncover trends and patterns that
+                            drive business strategies.
+                        </span>
+                        <br/><br/><br/>
+                        <span className="data_text">
+                            Data Analysts gather data from various sources, ensuring its quality and integrity.
+                            They clean and preprocess data, apply statistical analysis and data visualization
+                            techniques, and communicate insights effectively through reports and presentations.
+                        </span>
+                        <br/><br/><br/>
+                        <span className="data_text">
+                            Data Analysts drive data-informed decision-making processes by providing valuable insights
+                            that guide strategic planning and operational improvements. They empower businesses to make
+                            evidence-based decisions, monitor key performance indicators, and ensure agility in dynamic
+                            market environments.
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <br/><br/> <br/><br/><br/><br/><br/><br/>
+            <div className="many_more_div">
+                <span className="many_more">So many more</span>
+                <span className="many_more_text">
+                    Active collaboration with both the internal and external partners of your firm
+                    can help in the agile development of software products that are creative and flexible.
+                </span>
+            </div>
+            <br/><br/> <br/><br/><br/><br/> <br/><br/><br/><br/>
+            <div className="business_controler">
+                <div className="business_controler_part1">
+                    <span className="business_controler_title">Business controler</span>
+                    <br/><br/><br/><br/>
+                    <span className="business_controler_text">
+                        Consolidating all of your data, including digital documents stored in
+                        Team Folders, provides your team with a comprehensive overview of all your projects in one
+                        centralized location.
+                    </span>
+                </div>
+                <img src={bc_illu} className="bc_illu" alt="business Central illustration"/>
+            </div>
+            <br/><br/> <br/><br/><br/><br/> <br/><br/><br/><br/>
+        </div>
+    );
+}
+
+export default Finance;
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import './Header.css';
 import logo from '../Img/LogoTafili.png';
+import linkedinIcon from '../Img/icon_linkdin.png';
+import emailIcon from '../Img/icon_email.png';
+
 
 function Header() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -12,10 +15,19 @@ function Header() {
     return (
         <div className="Header">
             <header className="App-header">
+                <div className="contact-icons">
+                    <a href="https://www.linkedin.com/in/drilon-tafili-5a4599161/"><img src={linkedinIcon}
+                                                                                        alt="LinkedIn"
+                                                                                        className="contact-icon"/></a>
+                    <a href="mailto:contact@tafiliconsulting.be"><img src={emailIcon} alt="Email"
+                                                                      className="contact-icon"/></a>
+                </div>
+                <div className="work-with-me-button">
+                    <a href="/contact" className="button-travaille">Rejoignez-nous</a>
+                </div>
 
                 <nav id="navbar" className="">
                     <div className="nav-wrapper">
-
                         <div className="logo">
                             <a href="/"> <img src={logo} alt="Logo Tafili" id='imageLogo'/></a>
                         </div>
